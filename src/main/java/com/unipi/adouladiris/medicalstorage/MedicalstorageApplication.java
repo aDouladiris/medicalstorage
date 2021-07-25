ac3baa01cf16455accbe0a6dda09824d2277598c
aDouladiris <58791947+aDouladiris@users.noreply.github.com>
2021-07-25 14:04:19 +0300

    Initial commit

---
+++ src/main/java/com/unipi/adouladiris/medicalstorage/MedicalstorageApplication.java
@@ -0,0 +1,13 @@
+package com.unipi.adouladiris.medicalstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalstorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalstorageApplication.class, args);
+    }
+
+}
