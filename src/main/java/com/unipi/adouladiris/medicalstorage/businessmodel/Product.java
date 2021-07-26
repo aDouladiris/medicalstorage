0a513106bcd58b11a8d4735065c7d3b62c4c7c88
aDouladiris <58791947+aDouladiris@users.noreply.github.com>
2021-07-26 14:25:58 +0300

    Json format complete.

--- src/main/java/com/unipi/adouladiris/medicalstorage/businessmodel/Product.java
+++ src/main/java/com/unipi/adouladiris/medicalstorage/businessmodel/Product.java
@@ -2,7 +2,8 @@ package com.unipi.adouladiris.medicalstorage.businessmodel;
 
 import com.unipi.adouladiris.medicalstorage.entities.operable.*;
 import com.unipi.adouladiris.medicalstorage.entities.operable.abstractClass.Operable;
-import net.minidev.json.JSONObject;
+//import net.minidev.json.JSONObject;
+import org.json.simple.JSONObject;
 
 import java.util.*;
 
@@ -55,46 +56,53 @@ public class Product extends Operable {
         }
     }
 
-    public TreeMap getJson(){
+    public JSONObject getJson(){
 
-        TreeMap<String, TreeMap<String,TreeMap<String, TreeMap<String, TreeMap<String, TreeSet<String> >>>>> simplifiedProduct = new TreeMap<>();
-
+//        TreeMap<String, TreeMap<String,TreeMap<String, TreeMap<String, TreeMap<String, TreeSet<String> >>>>> simplifiedProduct = new TreeMap<>();
+        JSONObject substanceJO = new JSONObject();
         for( Substance substance: this.product.keySet() ){
+            Set<JSONObject> tabList = new HashSet<>();
             for( Tab tab : this.product.get(substance).keySet() ){
+                JSONObject tabJO = new JSONObject();
+                Set<JSONObject> categoryList = new HashSet<>();
                 for ( Category category : this.product.get(substance).get(tab).keySet() ){
+                    JSONObject categoryJO = new JSONObject();
+                    Set<JSONObject> itemList = new HashSet<>();
                     for ( Item item : this.product.get(substance).get(tab).get(category).keySet() ){
+                        JSONObject itemJO = new JSONObject();
+                        itemJO.put("Title", item.getName());
+                        Set<String> tagList = new HashSet<>();
                         for ( Tag tag : this.product.get(substance).get(tab).get(category).get( item ) ){
-
-
-                            TreeSet tagSet = new TreeSet();
-                            tagSet.add(item.getDescription());
-
-                            TreeMap itemMapDesc = new TreeMap();
-                            itemMapDesc.put(tag.getName(), tagSet );
-
-                            TreeMap itemMapName = new TreeMap();
-                            itemMapName.put(item.getName(), itemMapDesc );
-
-                            TreeMap catMap = new TreeMap();
-                            catMap.put(category.getName(), itemMapName);
-
-                            TreeMap tabMap = new TreeMap();
-                            tabMap.put(tab.getName(), catMap);
-
-//                            JSONObject jo = new JSONObject();
-//                            jo.put(substance.getClass().getSimpleName(), substance.getName());
-//
-                            simplifiedProduct.put(substance.getName(), tabMap);
-
-//                            Set<tmpObject> ll = new HashSet<>();
-//                            ll.add( new tmpObject(substance.getClass().getSimpleName(), substance.getName() ) );
-
+                            tagList.add(tag.getName());
                         }
+                        itemJO.put("Tag", tagList);
+                        itemJO.put("Descrption", item.getDescription());
+                        itemList.add(itemJO);
                     }
+//                    JSONObject catTmp = new JSONObject();
+                    ArrayList<Object> catTmp = new ArrayList();
+                    catTmp.add(category.getName());
+                    catTmp.add(itemList);
+                    categoryJO.put("Category", catTmp);
+//                    categoryJO.put(category.getName(), itemList);
+                    categoryList.add(categoryJO);
                 }
+//                JSONObject tabTmp = new JSONObject();
+                ArrayList<Object> tabTmp = new ArrayList();
+                tabTmp.add(tab.getName());
+                tabTmp.add(categoryList);
+                tabJO.put("Tab", tabTmp);
+//                tabJO.put(tab.getName(), categoryList);
+                tabList.add(tabJO);
             }
+//            JSONObject subTmp = new JSONObject();
+            ArrayList<Object> subTmp = new ArrayList();
+            subTmp.add(substance.getName());
+            subTmp.add(tabList);
+            substanceJO.put("Substance", subTmp);
+            substanceJO.put(substance.getName(), tabList);
         }
-        return simplifiedProduct;
+        return substanceJO;
     }
 
     public class tmpObject{
@@ -104,6 +112,32 @@ public class Product extends Operable {
             _key = k; _value = v;
         }
     }
+
+
+    //                            TreeSet tagSet = new TreeSet();
+//                            tagSet.add(item.getDescription());
+//
+//                            TreeMap itemMapDesc = new TreeMap();
+//                            itemMapDesc.put(tag.getName(), tagSet );
+//
+//                            TreeMap itemMapName = new TreeMap();
+//                            itemMapName.put(item.getName(), itemMapDesc );
+//
+//                            TreeMap catMap = new TreeMap();
+//                            catMap.put(category.getName(), itemMapName);
+//
+//                            TreeMap tabMap = new TreeMap();
+//                            tabMap.put(tab.getName(), catMap);
+//
+////                            JSONObject jo = new JSONObject();
+////                            jo.put(substance.getClass().getSimpleName(), substance.getName());
+////
+//                            simplifiedProduct.put(substance.getName(), tabMap);
+
+//                            Set<tmpObject> ll = new HashSet<>();
+//                            ll.add( new tmpObject(substance.getClass().getSimpleName(), substance.getName() ) );
+
+
 
 }
 
--- src/main/java/com/unipi/adouladiris/medicalstorage/rest/Controller.java
+++ src/main/java/com/unipi/adouladiris/medicalstorage/rest/Controller.java
@@ -1,6 +1,4 @@
 package com.unipi.adouladiris.medicalstorage.rest;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.unipi.adouladiris.medicalstorage.businessmodel.Product;
 import com.unipi.adouladiris.medicalstorage.database.operations.select.Select;
 import net.minidev.json.JSONObject;
@@ -23,17 +21,7 @@ public class Controller {
 
         Product product = new Select().findProduct("Aspirine").getResult(Product.class);
 
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-        JSONObject t1 = new JSONObject();
-        t1.put("key", "value");
-
-        JSONObject t2 = new JSONObject();
-        t2.put("Another", t1);
-
-        String json = gson.toJson(t2);
-
-        return t2.toJSONString();
+        return product.getJson().toJSONString();
 
         //return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
